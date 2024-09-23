d7aab0fce996f12a7d67d4f72e38b2afd46185f9
Mohssin El karrouti <166851789+mohssinhach@users.noreply.github.com>
2024-09-23 01:59:13 +0100

    Update live.js

--- js/live.js
+++ js/live.js
@@ -1,5 +1,5 @@
 var cp = " Gems";
-var codcp = ['800','800','700','800','600','800','500','700','800','800'];	
+var codcp = ['800','800','700','800','800','700','500','800','600','800'];	
 var names = ['1upCoop', 'Acquetwa4', 'AimWaka', 'Anemont2', 'Arcaloci18', 'Aurallyfun', 'Badgenema2', 'BeautySkill99', 'Cartridgeop', 'rgynaCaste', 'Cha2rpavi', 'Classoranta33', 'Combat122', 'Datatah', 'Datatech34', 'Emulatorch65', 'Emulatorte74', 'FiDungeon67', 'HStreaming93', 'Hopetailzz', 'Hylysackxx', 'xJoysIndiex', 'Leaguermof55', 'LeeWizardz3z', 'Lemnison42', 'Leucomm12', 'Lexondi66', 'LunaticDj4c', 'Lutingar2x' ,'Tournat199', 'TzTours', 'T2rinial', 'Unitara54'];	
 function create() {
 VanillaToasts.create({
@@ -7,7 +7,7 @@ VanillaToasts.create({
   text: document.body.valueToUse = codcp[Math.floor(Math.random() * codcp.length)] + cp,
   type: 'success',
   icon: 'https://i.postimg.cc/fR4qMrzz/profile.png',
-  timeout: 6000,
+  timeout: 2500,
 });
-setTimeout(create, 3000)
+setTimeout(create, 6000)
 }
